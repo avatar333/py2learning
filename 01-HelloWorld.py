8bece5c45f9da5a32543a49ea3ce713cf3cae6c2
avatar333 <kevin.pillay@gmail.com>
2017-01-11 14:56:10 +0200

    Initial commit
    Signed-off-by: avatar333 <kevin.pillay@gmail.com>

--- 01-HelloWorld.py
+++ 01-HelloWorld.py
@@ -1,1 +1,43 @@
-kkprint ("Hello World!\tkTest")k
+print ("Hello World!\tkTest")
+
+# Print the output of a calculation
+print (1+1)
+
+# Assign value of calcuation to a variable
+NUM1 = 1+1
+
+# print a string and variable
+print ("NUM1 = "), NUM1, "\n", NUM1
+
+
+# split a string, specifying a delimiter, and then which element
+print ("Hello World").split(" ")[0]
+
+# Boolean examples
+print 5 == 5
+
+print "this" is "This"
+print "this" is not "This"
+
+# Lists
+
+print ["This", "is", "a", "list"][0]
+
+# Dictionaries
+
+print {"Name": "Kevin", "Surname": "Pillay"}["Name"]
+
+# Function example 1
+
+def function_one():
+    print ("Function One")
+
+function_one()
+
+# Function example 2
+
+def function_two(str1, str2):
+    print (str1)
+    print (str2)
+
+function_two("String 1", "String 2")
